6844a5b74efaa1a078943abef0799846ad8805ec
liyi <liyiyu12@gmail.com>
2019-01-02 15:17:15 -0500

    add has_one_through method

--- associatable.rb
+++ associatable.rb
@@ -90,10 +90,31 @@ module Associatable
             through_options.model_class.assoc_options[source_name]
          
          through_table = through_options.table_name 
-         through_prim_key = through_options.primary_key
-         through_for_key = through_options.foreign_key
+         through_pkey = through_options.primary_key
+         through_fkey = through_options.foreign_key
 
          source_table = source_options.table_name
+         source_pkey = source_options.primary_key 
+         source_fkey = source_options.foreign_key 
+
+         key_val = self.send(through_fkey)
+         results = DBConnection.execute(<<-SQL, key_val)
+            SELECT 
+               #{source_table}.*
+            FROM 
+               #{through_table}
+            JOIN 
+               #{source_table}
+            ON
+               #{through_table}.#{source_fkey} = #{source_table}.#{source_pkey}
+            WHERE 
+               #{through_table}.#{through_pk} = ?
+         SQL
+
+         source_options.model_class.parse_all(results).first
+      end 
+   end 
+end 
 
 
 
